af53d7030c2ac505f4cce265e6f790f783691a7d
Sami Rantanen <68911600+childzplay@users.noreply.github.com>
2023-06-01 20:59:58 +0300

    Update template-functions.php

--- template-functions.php
+++ template-functions.php
@@ -14,4 +14,34 @@ function custom_logo_output() {
 	}
 }
 
+
+/** Block Anchor **/
+function get_anchor( $block_name ) {
+	$block_input = get_sub_field( $block_name );
+	if( !empty( $block_input ) ) { 
+		$anchor = 'id="' . $block_input . '" data-magellan-target="' . $block_input . '"'; } 
+	else {
+		$anchor = '';
+	}
+	return $anchor;
+}
+
+
+/** Block Title */
+function get_block_title($title, $tag, $classes) {
+	return '<'.$tag.' class="block-title '.$classes.'">'.$title.'</'.$tag.'>';
+}
+
+
+/** Theme Button **/
+function get_button( $link, $target, $style, $text ) {
+	$link = $link ? $link : '#';
+	$link_target = $target ? $target : '_self';
+	$text = $text ? $text : 'Lue lisää';
+
+	$button = '<a href="'.$link.'" target="'.$link_target.'" class="btn '.$style.'">'.$text.'</a>';
+	
+	return $button;
+}
+
 ?>
